fc7be7a98499adc9fad2395094e4791e2195a74f
hans-jorg <hans.schneebeli@gmail.com>
2020-08-03 00:57:16 -0300

    Completed 14-Newlib. Updated all Makefiles Added tty layer. Added read routines with line buffering Restructured 21-Temperature

---
+++ 06-ClockFrequency/main.c
@@ -0,0 +1,61 @@
+/** ***************************************************************************
+ * @file    main.c
+ * @brief   Simple LED Blink Demo for EFM32GG_STK3700
+ * @version 1.0
+******************************************************************************/
+
+#include <stdint.h>
+/*
+ * Including this file, it is possible to define which processor using command line
+ * E.g. -DEFM32GG995F1024
+ * The alternative is to include the processor specific file directly
+ * #include "efm32gg995f1024.h"
+ */
+#include "em_device.h"
+#include "led.h"
+
+#define DELAYVAL 1
+
+/**
+ * @brief  Quick and dirty delay function
+ * @note   Do not use in production code
+ */
+
+void Delay(uint32_t delay) {
+volatile uint32_t counter;
+int i;
+
+    for(i=0;i<delay;i++) {
+        counter = 1000000;
+        while( counter ) counter--;
+    }
+}
+
+/**
+ * @brief  Main function
+ *
+ * @note   Using default clock configuration
+ *         HFCLK = HFRCO
+ *         HFCORECLK = HFCLK
+ *         HFPERCLK  = HFCLK
+ */
+
+int main(void) {
+
+    /* Configure Pins in GPIOE */
+    LED_Init(LED1|LED2);
+
+    /* Blink loop */
+    while (1) {
+
+        Delay(DELAYVAL);
+        LED_Toggle(LED1);                                // Toggle LED1
+
+        Delay(DELAYVAL);
+        LED_Toggle(LED2);                                // Toggle LED2
+
+        Delay(DELAYVAL);
+        LED_Write(0,LED1|LED2);                          // Turn On All LEDs
+
+    }
+}
